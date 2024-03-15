7f6435821369303f12faf4c2bad34c441311d38f
Gaurav <gauravv.bhainsora@gmail.com>
2024-03-15 13:07:42 +0530

    python

--- src/addition.py
+++ src/addition.py
@@ -1,9 +1,4 @@
-# app.py
-# This is a test commit
-def add(a, b):
-    return a + b
-
-def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
-    #test
+num1 = 5
+num2 = 10
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum)
